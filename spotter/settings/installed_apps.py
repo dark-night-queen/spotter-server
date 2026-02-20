efd47e3c2b27b826d1e2d3ae82ec80305fccde6c
Shreya <agrawal.shreya@icloud.com>
2026-02-20 22:32:07 +0530

    added base files

---
+++ spotter/settings/installed_apps.py
@@ -0,0 +1,17 @@
+# Application definition
+DEFAULT_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+THIRD_PARTY_APPS = [
+    "corsheaders",
+    "rest_framework",
+]
+
+
+SELF_APPS = ["apps.trip"]
