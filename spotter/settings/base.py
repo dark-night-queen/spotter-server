324b4323d5767e64d7ee0b48b5dc0b147cf8862f
Shreya <agrawal.shreya@icloud.com>
2026-02-22 21:14:31 +0530

    fixed , in base.py

--- spotter/settings/base.py
+++ spotter/settings/base.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = DEFAULT_APPS + SELF_APPS + THIRD_PARTY_APPS
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware"
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
